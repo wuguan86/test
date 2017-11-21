700e2cb5b15ecdb0e65414cf41671604c407b0da
wuguan86 <591004761@qq.com>
2017-11-22 07:37:05 +0800

    sa

--- test.java
+++ test.java
@@ -2,12 +2,10 @@ public  class test{
 
 
 
-<<<<<<< HEAD
-  system.out.pringln("masterssss");
-=======
+
 
   system.out.pringln("masterss");
->>>>>>> 7e0bd1e9d6227a59cf9a0fc0cbe0e44828764386
+
 
 
 
