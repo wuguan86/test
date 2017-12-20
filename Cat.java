d1cb9601c67826fc3a755af10accd419683fd2a6
wuguan86 <591004761@qq.com>
2017-12-21 00:49:46 +0800

    dsa

--- Cat.java
+++ Cat.java
@@ -4,16 +4,9 @@ public class Cat extends Animal {
 
 	@Override
 	public void eat() {
-<<<<<<< HEAD
-
-
 
 		System.out.println("猫吃鱼2");
 
-
-=======
-		System.out.println("猫吃鱼one");
->>>>>>> remotes/origin/onebanch
 	}
 
 }
